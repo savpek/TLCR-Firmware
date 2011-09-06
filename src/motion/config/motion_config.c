002be2ce600f30fa6eb8716af33eeefc301281ab
Savpek Electronics <savpekelectronics@gmail.com>
2011-09-07 00:59:32 +0300

    Initial commit of motion service. Motion service controls variable amount of steppermotors.
    Also just fixed ISSUE #0 which prevented usage of service with portB.
    
    Signed-off-by: Savpek Electronics <savpekelectronics@gmail.com>

---
+++ src/motion/config/motion_config.c
@@ -0,0 +1,52 @@
+/*
+ * motion_motor_config.c
+ *
+ * Created: 28.8.2011 15:50:30
+ *  Author: savpek
+ */ 
+
+#include "motion_config.h"
+#include "compiler.h"
+
+const motorc_step_t motion_step_sequence[] = {
+ /*  A-coil     B-coil		<-PWMA value */
+	{-127,		-127},		//STEP 0
+	{127,		-127},		//STEP 1
+	{127,		127},		//STEP 2
+	{-127,		127},		//STEP 3
+};
+
+const motorc_step_t motion_step_sleep_sequence[] = {
+ /*  A-coil     B-coil		<-PWMA value */
+	{-90,		-90},		//STEP 0
+	{90,		-90},		//STEP 1
+	{90,		90},		//STEP 2
+	{-90,		90},		//STEP 3
+};
+
+/* Settings for motor which rotates camera platform */
+volatile motorc_t motion_handle[] =
+	{
+	/* MOTION_ID_RMOTOR (0): ... */
+		{
+		/* Define physical pins for PWMA */
+		.coil_a_pos_pwma_pin = AVR32_PWMA_28_PIN,	/* Often RED wire*/
+		.coil_a_neg_pwma_pin = AVR32_PWMA_29_PIN,	/* Often BLUE wire*/
+		.coil_b_pos_pwma_pin = AVR32_PWMA_30_PIN,	/* Often YELLOW wire*/
+		.coil_b_neg_pwma_pin = AVR32_PWMA_31_PIN,	/* Often WHITE wire*/
+
+		/* Define PWMA channel for each coil pin */
+		.coil_a_pos_pwma_channel = 28,
+		.coil_a_neg_pwma_channel = 29,
+		.coil_b_pos_pwma_channel = 30,
+		.coil_b_neg_pwma_channel = 31,
+		.pwma_function = AVR32_PWMA_15_FUNCTION,
+	
+		/* We don't want that motor suddenly moves in beginning. */
+		.motor_direction = STOP,
+	
+		/* Set initial value to zero. */
+		.step_count = 0,
+		.step_delay_ms = 0
+		}
+	};
---
+++ src/motion/inc/motion.h
@@ -0,0 +1,45 @@
+/*
+ * motion.h
+ *
+ * Created: 8.8.2011 20:02:02
+ *  Author: savpek
+ */ 
+
+#ifndef MOTION_H_
+#define MOTION_H_
+
+#include "compiler.h"
+#include "motion_config.h"
+#include "error_codes.h"
+
+/* Inits necessary system components for motion library. EG: Clocks and interrupts. */
+void motion_init (void);
+
+/*!! Go see source file motion_motor_config.c to config motors used. 
+ * In this array, you define all motor parameters and it also includes
+ * handle variables that can be written from elsewhere (like change speed and
+ * direction. !!*/
+extern volatile motorc_t motion_handle[];
+
+/*@ This function tries take control of motor. */
+extern errorc_t motion_try_take_control(uint8_t motion_handle_id, motorc_ctrl_unit_t);
+
+/*@ This function take control over motor, no matter what unit
+ *  is currently controlling it. */
+extern void motion_force_take_control(uint8_t motion_handle_id, motorc_ctrl_unit_t);
+
+/*@ Checks that which unit is currently using motors */
+extern motorc_ctrl_unit_t motion_check_control(uint8_t motion_handle_id);
+
+/*@ Release control of unit */
+extern void motion_release_control(uint8_t motion_handle_id);
+
+/*@ Sets speed of selected motor.
+ *	Units per h are mm/h in X movement, and 1/100 degrees per h
+ *	in rotation movement. */
+extern void motion_set_rotate_speed(uint8_t motion_handle_id, uint32_t hundred_of_degree_per_h);
+extern void motion_set_move_speed(uint8_t motion_handle_id, uint32_t mm_per_h);
+
+/*@ Set direction of selected motor. */
+extern void motion_set_direction( uint8_t motion_handle_id, motorc_dir_t direction);
+#endif /* MOTION_H_ */
